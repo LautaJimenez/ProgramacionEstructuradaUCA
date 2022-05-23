326f9fa0e78884a87682ac364ef9ca9e3d23ed4e
LautaJimenez <lauta.jimenez@gmail.com>
2022-05-23 15:13:01 -0300

    Ejercicios recursividad

---
+++ Practicas/Practica4/Ejercicio4/Ejercicio4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+/*
+void invertirRecursivo(int *arreglo, int largo){
+
+
+
+}
+*/
+
+
+void imprimirArreglo(int *arreglo){
+
+
+
+    
+}
+int main(){
+
+    int arreglo[6] = {1,2,3,4,5,6};
+    return 0;
+}
