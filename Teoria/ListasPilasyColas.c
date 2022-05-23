1ce575b0368e274b22fac6d3a53f971d3da6cc29
LautaJimenez <lauta.jimenez@gmail.com>
2022-05-23 14:25:03 -0300

    ''

---
+++ Teoria/ListasPilasyColas.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// RECORDAR: Hacer ejercicios de insertar un elemento ordenado para parcialito (Lo dijo Mariano). Por ejemplo: Tengo una lista A,B,D,E -> Insertar -> A,B,C,D,E.
+
+// Listas:
+
+// Una lista es una una estructura secuencial de datos. Cada uno de sus datos, son estructuras, y se definen como nodos.
+// Un nodo es una estructura que tienen 2 elementos -> El contenido de la información, y el puntero al siguiente nodo.
