d506d5d40cc687f793e5eff3c7bc183f94264abe
LautaJimenez <lauta.jimenez@gmail.com>
2022-05-24 19:11:12 -0300

    Ejercicios recursividad

---
+++ Practicas/Practica4/Ejercicio5/Ejercicio5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+
+#define TERMINADOR 0
+
+int maximoRecursivo(int *arreglo){
+    
+
+}   
+
+
+int main(){
+
+    arreglo[6] = {1,2,3,4,5,TERMINADOR};
+    return 0;
+}
