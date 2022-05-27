02113f7bbc8591e2b9d2a67ec9a6759a4126526b
LautaJimenez <lauta.jimenez@gmail.com>
2022-05-27 12:16:24 -0300

    Pilas: Clase 27/05

--- Teoria/ListasPilasyColas.c
+++ Teoria/ListasPilasyColas.c
@@ -3,7 +3,23 @@
 
 // RECORDAR: Hacer ejercicios de insertar un elemento ordenado para parcialito (Lo dijo Mariano). Por ejemplo: Tengo una lista A,B,D,E -> Insertar -> A,B,C,D,E.
 
+// Los arreglos, tanto en la memoria estatica como dinamica, se pueden recorrer con un for o un while, ya que estan consecutivos en la memoria. Las listas NO.
+
+// Clase 20/05
+
 // Listas:
 
 // Una lista es una una estructura secuencial de datos. Cada uno de sus datos, son estructuras, y se definen como nodos.
 // Un nodo es una estructura que tienen 2 elementos -> El contenido de la información, y el puntero al siguiente nodo.
+
+// Clase 27/05
+
+// Pilas:
+
+// Las pilas tienen 2 funciones: pop y push. Unicamente se accede a una pila mediante alguna de las dos funciones mencionadas. 
+
+// Las pilas se llenan de arriba, y se sacan de arriba. Es como una "escalera". El ultimo en entrar es el primero en salir.
+
+// Push: mete el elemento en la pila. (Es el insertarAdelante en listas). (append en python).
+// Pop: saca el elemento de la pila. Para imprimir una pila, se debe usar el pop para retornar el valor. (pop en python)
+
