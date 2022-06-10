a9e1702620ff49bc9d616f97bca97b210ed0e7ce
LautaJimenez <lauta.jimenez@gmail.com>
2022-06-10 12:01:12 -0300

    Archivos binarios

--- Teoria/ArchivosBinarios/ArchivosBinarios.c
+++ Teoria/ArchivosBinarios/ArchivosBinarios.c
@@ -6,6 +6,8 @@
 // fread (Lee el archivo).
 // fclose (Cierra el archivo).
 
+// ____________________________________________________________________________________________________________________________________________________________________
+
 // Modos de apertura:
 
     //  rb -> read en binario.
@@ -13,4 +15,21 @@
     //  w + b -> Crea un archivo vacio en binario, para poder escribir y leer.
     //  r + b -> Abre un archivo en binario, para poder escribir y leer.
 
-    
+// ____________________________________________________________________________________________________________________________________________________________________
+
+// Formas de fwrite y fread:
+
+    // fwrite:
+
+        // size_t fwrite ( const void * restrict buffer, size_t size, size_t count, FILE * restrict stream ) ; 
+        //		  fwrite (       fuentedeDatos         ,       size ,   count     ,      archivo           ) ; 
+
+    // fread:
+
+        // size_t fread  ( void * restrict buffer, size_t size, size_t count, FILE * restrict stream ) ; 
+		//	      fread (      destinodeDatos   ,       size ,   count     ,      archivo           ) ; 
+
+    // Size es el tamanio de lo que queres leer o escribir, y count es cuantas veces queres "trabajar" ese tamanio. Ejemplo: size: 50bytes, count: 2. Total: 100bytes.
+
+// ____________________________________________________________________________________________________________________________________________________________________
+
