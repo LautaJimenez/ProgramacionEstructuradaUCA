7b0ca66dbc1dc61f4d05e483f9b84883b64d9c5f
LautaJimenez <lauta.jimenez@gmail.com>
2022-06-10 10:52:28 -0300

    Archivos binarios

---
+++ Teoria/ArchivosBinarios/ArchivosBinarios.c
@@ -0,0 +1,16 @@
+// Funciones:
+
+// fopen (Abrir archivo).
+// fwrite (Escribir archivo).
+// rewind.
+// fread (Lee el archivo).
+// fclose (Cierra el archivo).
+
+// Modos de apertura:
+
+    //  rb -> read en binario.
+    //  wb -> write en binario.
+    //  w + b -> Crea un archivo vacio en binario, para poder escribir y leer.
+    //  r + b -> Abre un archivo en binario, para poder escribir y leer.
+
+    
